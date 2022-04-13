de7c9ed8d590f883faf1700ecd0a1a53443f42f9
daymickcorr <mickeycorriveau@hotmail.com>
2022-04-13 19:42:20 -0400

    Cleanup

--- mygame/web/urls.py
+++ mygame/web/urls.py
@@ -7,16 +7,13 @@ help.
 from django.conf.urls import url, include
 from django.urls import path
 
-#add web directory to pythonpath to support standard django
-import sys
-sys.path.append('/home/vagrant/mygame/web')
-
 # default evennia patterns
 from evennia.web.urls import urlpatterns
 
 # eventual custom patterns
 custom_patterns = [
     # url(r'/desired/url/', view, name='example'),
+    path('character/', include('web.character.urls'))
 ]
 
 # this is required by Django.
